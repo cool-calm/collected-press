44236444ca9872c11cf5c91565902baa7f65bdcb
Patrick Smith <patrick.smith@cultureamp.com>
2022-12-02 16:14:32 +1100

    Try to get parcel build happy again

--- packages/press-server/src/encodings.ts
+++ packages/press-server/src/encodings.ts
@@ -1,4 +1,4 @@
-export function encodeHex(buffer: ArrayBuffer) {
+export function encodeHex(buffer: ArrayBuffer): string {
   return Array.from(new Uint8Array(buffer), (uint8) =>
     uint8.toString(16).padStart(2, '0'),
   ).join('')
