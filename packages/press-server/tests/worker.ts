1ded4cc061f6ddb470e29d9d973072d8a5d32526
Patrick Smith <patrick.smith@cultureamp.com>
2022-12-02 17:14:46 +1100

    Bump to 0.2.0

--- packages/press-server/tests/worker.ts
+++ packages/press-server/tests/worker.ts
@@ -2,7 +2,6 @@ import { serveRequest } from "../src/index";
 
 export default {
     async fetch(request: Request) {
-        const { pathname } = new URL(request.url);
-        return await serveRequest("RoyalIcing", "RoyalIcing", pathname);
+        return await serveRequest("RoyalIcing", "RoyalIcing", new URL(request.url));
     }
 }
