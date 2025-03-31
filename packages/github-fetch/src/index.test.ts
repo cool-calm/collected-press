7870ba49284f73de5205e33fa7f17a689d61af81
Patrick Smith <pgwsmith@gmail.com>
2025-03-31 16:29:47 +1100

    Change test to point at this repo

--- packages/github-fetch/src/index.test.ts
+++ packages/github-fetch/src/index.test.ts
@@ -4,33 +4,31 @@ import {
   findHEADInRefs,
 } from './index'
 
+const owner = 'cool-calm'
+const repo = 'collected-press'
+
 test('fetchGitHubRepoRefs', async () => {
-  const refsGen = await fetchGitHubRepoRefs('RoyalIcing', 'RoyalIcing')
+  const refsGen = await fetchGitHubRepoRefs(owner, repo)
   const refsArray = Array.from(refsGen())
   expect(refsArray[0].ref).toEqual('HEAD')
-  expect(refsArray[0].HEADRef).toEqual('refs/heads/master')
+  expect(refsArray[0].HEADRef).toEqual('refs/heads/main')
 
   const headRef = findHEADInRefs(refsArray)
-  expect(headRef.HEADRef).toEqual('refs/heads/master')
-  expect(headRef.branch).toEqual('master')
+  expect(headRef.HEADRef).toEqual('refs/heads/main')
+  expect(headRef.branch).toEqual('main')
 })
 
 test('fetchGitHubRepoContent', async () => {
-  const refsGen = await fetchGitHubRepoRefs('RoyalIcing', 'RoyalIcing')
+  const refsGen = await fetchGitHubRepoRefs(owner, repo)
   const refsArray = Array.from(refsGen())
   const headRef = findHEADInRefs(refsArray)
   if (!headRef) throw Error('No HEAD to be found.')
 
   const { sha } = headRef
-  const homeRes = await fetchGitHubRepoContent(
-    'RoyalIcing',
-    'RoyalIcing',
-    sha,
-    'README.md',
-  )
+  const homeRes = await fetchGitHubRepoContent(owner, repo, sha, 'README.md')
   expect(homeRes).toBeInstanceOf(Response)
   expect(homeRes.status).toBe(200)
 
   const html = await homeRes.text()
-  expect(html).toContain('# Patrick George Wyndham Smith')
+  expect(html).toContain('# Collected Press')
 })
